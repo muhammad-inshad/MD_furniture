1ee4903bd8f262a12621cd311e77b2c476c164d5
inshad <minshad430@gmail.com>
2025-03-13 17:21:05 +0530

    week 12

--- router/userRouter.js
+++ router/userRouter.js
@@ -44,28 +44,24 @@ router.post("/wishlist",userContoller.wishlist)
 router.get("/showWishlist",isLoginORnot,userContoller.showWishlist)
 router.post("/WishlistToggle",userContoller.WishlistToggle)
 
-router.get("/auth/google", passport.authenticate("google", { scope: ['profile', 'email'] }));
-
-router.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: '/user/signup' }), async (req, res) => {
-    try {
-        const findemail = await User.findOne({ email: req.user.email });
-        if (findemail.isBlocked === true) {
-            return res.status(400).json({ success: false, message: "This user is blocked" });
-        }
-        if (!findemail) {
-            return res.status(401).json({ success: false, message: "Invalid email or password." });
-        }
-        req.session.user = {
-            id: findemail._id,
-            email: findemail.email,
-            isBlocked: findemail.isBlocked
-        };
-        return res.render("home", { success: true, message: "Login successful", isLogin: true });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ success: false, message: "An error occurred" });
+router.get("/auth/google",passport.authenticate("google",{scope:['profile','email']}))
+router.get("/auth/google/callback",passport.authenticate("google",{failureRedirect:'/user/signup'}),async(req,res)=>{
+    const findemail = await User.findOne({ email:req.user.email });
+    if (findemail.isBlocked === true) {
+        return res.status(400).json({ success: false, message: "this user is blocked" })
     }
-});
+    if (!findemail) {
+        return res.status(401).json({ success: false, message: "Invalid email or password." });
+    }
+    req.session.user = {
+        id: findemail._id,
+        email: findemail.email,
+        isBlocked: findemail.isBlocked
+    }
+    return res.render("home",{ success: true, message: "Login successful", isLogin: true });
+
+
+})
 router.get("/chair",isUser,uploadFields,chairControler.chair)
 router.get("/showDetailProduct/:id",isUser,uploadFields,sofaController.showDetailProduct)
 
