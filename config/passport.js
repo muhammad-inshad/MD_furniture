71aa924adf0a3066d004e16c207b43b153215b45
inshad <minshad430@gmail.com>
2025-03-18 00:44:34 +0530

    redirection changed to home page

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const env=require("dotenv").config
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://mdfurniture.zapto.org/auth/google/callback"  // Need a specific path
+  callbackURL: "http://mdfurniture.zapto.org/user/home"  // Need a specific path
 }, 
 
 async(accessToken,refreshToken,profile,done)=>{
